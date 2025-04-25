f615ba04921f1c1eddfc73aecddcf0a524eec9ec
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-04-25 11:32:42 +0600

    delete api create

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 //middleware
@@ -42,10 +42,20 @@ async function run() {
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem);
             res.send(result);
+        })  
+
+        // finds all cart items for a specific user by email
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
         })
 
-        app.get('/carts', async (req, res) => {
-            const result = await cartCollection.find().toArray();
+        app.delete('/carts/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
